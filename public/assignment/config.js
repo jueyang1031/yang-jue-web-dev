40ee07c8ef7c635a5d1c4571c0cf9183cf752c60
jueyang1031 <jueyang@ccs.neu.edu>
2016-05-25 19:39:25 -0400

    move user to views, refactor to html fragments, add app.js, config ng-route, create single page app on index.htmal

---
+++ public/assignment/config.js
@@ -0,0 +1,27 @@
+/**
+ * Created by yangjue on 5/25/16.
+ */
+(function(){
+    angular
+        .module("WebAppMaker")
+        .config(Config);
+
+    function Config($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "views/user/login.view.client.html"
+            })
+            .when("/login", {
+                templateUrl: "views/user/login.view.client.html"
+            })
+            .when("/register", {
+                templateUrl: "views/user/register.view.client.html"
+            })
+            .when("/profile", {
+                templateUrl: "views/user/profile.view.client.html"
+            })
+            .when("/profile", {
+                templateUrl: "views/user/profile.view.client.html"
+            })
+    }
+})();
